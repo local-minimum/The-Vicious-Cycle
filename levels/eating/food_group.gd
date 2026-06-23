3c21ac56062f7763dd97d4134cb00e20f22f4984
Martin Zackrisson <martin.zackrisson@fripost.org>
2026-06-23 10:05:01 +0200

    Hook up all fridge food

--- levels/eating/food_group.gd
+++ levels/eating/food_group.gd
@@ -3,6 +3,7 @@ class_name FoodGroup
 
 @export var pieces: Array[Node2D]
 @export var food: Food2D
+@export var click_to_eat: bool = true
 
 static var _focus_food: FoodGroup
 var _clicked_focus: bool
@@ -10,12 +11,15 @@ var _clicked_focus: bool
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
     if event is InputEventMouseButton:
         var m_event: InputEventMouseButton = event
-        if m_event.button_index != MOUSE_BUTTON_LEFT:
+        if m_event.is_echo() || !m_event.is_pressed() || m_event.button_index != MOUSE_BUTTON_LEFT:
             return
     else:
         return
 
-    if _focus_food == self:
+    if click_to_eat:
+        _handle_eat()
+        __SignalBus.on_inspect_food.emit(food, pieces.size(), _handle_eat)
+    elif _focus_food == self:
         _clicked_focus = true
     else:
         _focus_food = self
@@ -37,6 +41,10 @@ func _handle_eat() -> void:
     if p != null:
         p.queue_free()
         __SignalBus.on_eat.emit(food)
+    if pieces.is_empty():
+        visible = false
+        await get_tree().create_timer(0.25).timeout
+        __SignalBus.on_stop_inspect_food.emit()
 
 func _on_mouse_exited() -> void:
     if !_clicked_focus && _focus_food == self:
