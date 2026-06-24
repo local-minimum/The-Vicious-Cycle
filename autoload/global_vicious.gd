4f525d7077c5208b2c63615b84f3968ab3a2152f
Martin Zackrisson <martin.zackrisson@fripost.org>
2026-06-24 23:36:38 +0200

    Start on passage of time

--- autoload/global_vicious.gd
+++ autoload/global_vicious.gd
@@ -1,11 +1,15 @@
 class_name GlobalStateVicious
 
+static var day: int = 0
+static var time_checkpoint: int = -1
 static var door_cylinder_offsets: Array[int] = [0, 0, 0]
 static var crisis_counter: int = 0
 static var calories: float = 65.0
 static var happiness: float = 35.0
 static var eaten_food: Dictionary[String, int] = {}
 
+const _KEY_DAY: String = "day"
+const _KEY_TIME_CHECKPOINT: String = "time_chekpt"
 const _KEY_DOOR_CYLINDER: String = "door_cylinder_offsets"
 const _KEY_CRISIS: String = "cricis_counter"
 const _KEY_CALORIES: String = "calories"
@@ -14,6 +18,8 @@ const _KEY_EATEN: String = "eaten_food"
 
 static func save() -> Dictionary:
     return {
+        _KEY_DAY: day,
+        _KEY_TIME_CHECKPOINT: time_checkpoint,
         _KEY_DOOR_CYLINDER: door_cylinder_offsets,
         _KEY_CRISIS: crisis_counter,
         _KEY_CALORIES: calories,
@@ -22,6 +28,8 @@ static func save() -> Dictionary:
     }
 
 static func reset() -> void:
+    day = 0
+    time_checkpoint = -1
     door_cylinder_offsets = [0, 0, 0]
     crisis_counter = 0
     calories = 65.0
@@ -30,6 +38,8 @@ static func reset() -> void:
 
 static func load(data: Dictionary) -> void:
     var warn: bool = !data.is_empty()
+    day = DictionaryUtils.safe_geti(data, _KEY_DAY, 0, warn)
+    time_checkpoint = DictionaryUtils.safe_geti(data, _KEY_TIME_CHECKPOINT, 0, warn)
     door_cylinder_offsets = DictionaryUtils.safe_geta(data, _KEY_DOOR_CYLINDER, [0, 0, 0] as Array[int], warn)
     crisis_counter = DictionaryUtils.safe_geti(data, _KEY_CRISIS, 0, warn)
     calories = DictionaryUtils.safe_getf(data, _KEY_CALORIES, 65.0, warn)
---
+++ levels/passage_of_time/time_wheel.gd
@@ -0,0 +1,110 @@
+extends Node2D
+
+@export var _wheel: Node2D
+@export var _bed: Node2D
+@export var _short_sleep_bed: Node2D
+
+@export var _night_line: Node2D
+@export var _night_line_start_angle: float = -50.0
+
+@export var _night_exercise: Node2D
+
+@export var _short_night_tween_duration: float = 1.0
+
+@export var _first_day_angle_start: float = -164.0
+@export var _checkpoint_time_angles: Array[float]
+
+@export var _angle_rotation_duration: float = 0.1
+@export var _wait_before_next_scene: float = 1.0
+
+const _PT_WAKUP_FIRST_EXERCISE: int = 0
+const _PT_BREAKFAST: int = 1
+const _PT_SECOND_EXERCISE: int = 2
+const _PT_LUNCH: int = 3
+const _PT_DOOR_CHECK: int = 4
+const _PT_THIRD_EXERCISE: int = 5
+const _PT_SUPPER: int = 6
+const _PT_ATTEMPT_SLEEP: int = 7
+const _PT_NIGHT_EXERCISE_OR_SLEEP: int = 8
+const _PT_SHORT_SLEEP: int = 9
+
+func _ready() -> void:
+    if GlobalStateVicious.time_checkpoint < _PT_WAKUP_FIRST_EXERCISE:
+        _wheel.rotation_degrees = _first_day_angle_start
+    else:
+        _wheel.rotation_degrees = _checkpoint_time_angles[posmod(GlobalStateVicious.time_checkpoint - 1, 10)]
+    _night_line.visible = false
+    _night_exercise.visible = false
+    await get_tree().create_timer(_wait_before_next_scene).timeout
+    progress_time()
+
+func progress_time() -> void:
+    var next_time: int = posmod(GlobalStateVicious.time_checkpoint + 1, 10)
+
+    if next_time == 0:
+        GlobalStateVicious.day += 1
+
+    var target_a: float = _checkpoint_time_angles[next_time]
+    var delta: float = fposmod(_wheel.rotation_degrees - target_a, 180)
+    print_debug([_wheel.rotation_degrees, target_a, delta, _wheel.rotation_degrees + delta])
+    var duration: float = absf(delta) * _angle_rotation_duration
+
+    GlobalStateVicious.time_checkpoint = next_time
+
+    var t: Tween = create_tween()
+    t.tween_property(_wheel, "rotation_degrees", _wheel.rotation_degrees + delta, duration)
+    t.finished.connect(_handle_reach_checkpoint)
+
+var _short_sleep: bool = false
+
+func _handle_reach_checkpoint() -> void:
+    match GlobalStateVicious.time_checkpoint:
+        _PT_BREAKFAST, _PT_LUNCH, _PT_SUPPER:
+            await get_tree().create_timer(_wait_before_next_scene).timeout
+            push_warning("Don't know how to load eating")
+
+        _PT_ATTEMPT_SLEEP:
+            if GlobalStateVicious.happiness < 20.0 || GlobalStateVicious.calories > 60.0:
+                _short_sleep = true
+                tween_to_short_sleep()
+                progress_time()
+            else:
+                _short_sleep = false
+                progress_time()
+
+        _PT_SECOND_EXERCISE, _PT_THIRD_EXERCISE:
+            await get_tree().create_timer(_wait_before_next_scene).timeout
+            push_warning("Don't know how to load exercise")
+
+        _PT_NIGHT_EXERCISE_OR_SLEEP:
+            if _short_sleep:
+                await get_tree().create_timer(_wait_before_next_scene).timeout
+                push_warning("Don't know how to load exercise")
+            else:
+                await get_tree().create_timer(_wait_before_next_scene).timeout
+                push_warning("Don't know how to load sleep")
+
+        _PT_SHORT_SLEEP:
+            await get_tree().create_timer(_wait_before_next_scene).timeout
+            push_warning("Don't know how to load sleep")
+
+        _PT_DOOR_CHECK:
+            await get_tree().create_timer(_wait_before_next_scene).timeout
+            push_warning("Don't know how to load door check")
+
+        _PT_WAKUP_FIRST_EXERCISE:
+            await get_tree().create_timer(_wait_before_next_scene).timeout
+            push_warning("Don't know how to load wakeup")
+
+func tween_to_short_sleep() -> void:
+    _night_line.rotation_degrees = _night_line_start_angle
+    _night_line.visible = true
+
+    var t: Tween = create_tween()
+    t.set_parallel()
+    t.tween_property(_night_line, "rotation_degrees", 0.0, _short_night_tween_duration)
+    t.tween_property(_bed, "position", _short_sleep_bed.position, _short_night_tween_duration)
+    t.tween_property(_bed, "rotation", _short_sleep_bed.rotation, _short_night_tween_duration)
+    t.finished.connect(func() -> void:
+        _night_exercise.visible = true
+    )
